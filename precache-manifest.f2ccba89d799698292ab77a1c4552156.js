388ba54851d4cc31fb3201ce7d3357949d91466e
Ariel Gerstein <ariel.gers@gmail.com>
2021-12-29 23:39:25 -0300

    Updates

---
+++ precache-manifest.f2ccba89d799698292ab77a1c4552156.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfe263b4f8ef044d5368376a9e393df0",
+    "url": "/generative-art/index.html"
+  },
+  {
+    "revision": "5ff1854b788c3dbc8347",
+    "url": "/generative-art/static/css/main.437aa650.chunk.css"
+  },
+  {
+    "revision": "b7691974d63980413b4f",
+    "url": "/generative-art/static/js/2.f24b04e1.chunk.js"
+  },
+  {
+    "revision": "ebc14457535110648808604cc4b96810",
+    "url": "/generative-art/static/js/2.f24b04e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ff1854b788c3dbc8347",
+    "url": "/generative-art/static/js/main.35c23ff3.chunk.js"
+  },
+  {
+    "revision": "b4b2dd0d14d3ff26d8f5",
+    "url": "/generative-art/static/js/runtime-main.3a607440.js"
+  }
+]);
