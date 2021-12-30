40f88c5cd44c1d4452107dbd32fb1d91fb52e41f
Ariel Gerstein <ariel.gers@gmail.com>
2021-12-30 00:04:04 -0300

    Add list of sketches

--- src/App.js
+++ src/App.js
@@ -1,36 +1,64 @@
 import React from "react";
 import P5Wrapper from "react-p5-wrapper";
-import "./App.css";
+import styles from "./App.module.css";
 
 import shader1 from "./sketches/shader-1";
 import noiseMidi from "./sketches/noise-midi";
 import noise from "./sketches/noise";
 import mouse from "./sketches/mouse";
 import loop from "./sketches/loop";
-import blendMode from "./sketches/blendMode";
-import shapes from "./sketches/shapes";
 
-const sketches = [shader1, noiseMidi, noise, mouse, loop, blendMode, shapes];
+const sketches = [
+  {
+    title: "mouse interaction",
+    sketch: mouse,
+  },
+  {
+    title: "shapes",
+    sketch: loop,
+  },
+  {
+    title: "midi",
+    sketch: noiseMidi,
+  },
+  {
+    title: "noise",
+    sketch: noise,
+  },
+  {
+    title: "shader test",
+    sketch: shader1,
+  },
+];
 
 function App() {
-  const showButton = false;
-
-  const [currentSketch, setCurrentSketch] = React.useState(0);
-
-  const moveToNextSketch = React.useCallback(() => {
-    setCurrentSketch((currentSketch) =>
-      currentSketch + 1 >= sketches.length ? 0 : currentSketch + 1
-    );
-  }, []);
+  const [currentSketch, setCurrentSketch] = React.useState(null);
 
   return (
-    <div className="App">
-      {showButton && (
-        <button className="next-btn" onClick={moveToNextSketch}>
-          Current sketch: {currentSketch + 1}/{sketches.length} | Next >>
-        </button>
+    <div className={styles.container}>
+      {typeof currentSketch === "number" ? (
+        <>
+          <button
+            className={styles.goBackBtn}
+            onClick={() => setCurrentSketch(null)}
+          >
+            take me back
+          </button>
+          <P5Wrapper sketch={sketches[currentSketch].sketch} />
+        </>
+      ) : (
+        <div className={styles.intro}>
+          <h1>🌎 / generative art </h1>
+          {sketches.map(({ title, sketch }, i) => (
+            <div className={styles.sketchItem}>
+              <h2>{title}</h2>
+              <button onClick={() => setCurrentSketch(i)}>
+                show me now!!!
+              </button>
+            </div>
+          ))}
+        </div>
       )}
-      <P5Wrapper sketch={sketches[currentSketch]} />
     </div>
   );
 }
